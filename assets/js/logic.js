2d3e09fe92e8fd5fc07fd1b6cdae4bf74aea3ad7
AlessioMigotti <migottialessiodev@gmail.com>
2023-02-20 17:25:13 +0000

    looking at alternative storage of questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,13 @@
 
 
 
-
+/* 
 
 function init() {
 
     //setup all the button listeners
 
-    document.getElementById('submit').addEventListener('click'goToNext)          
+    document.getElementById('submit').addEventListener('click' goToNext)          
 
     // setup the question array
     var questions = ["Question1 dsflkfdskjfdskjl", "question 2 sdffdsfdsfds", "question 3 sdffdsfdsfds", "question 4 sdffdsfdsfds"];
@@ -16,9 +16,7 @@ function init() {
 
 }
 
-function goToNext() {
-     
-}
+
 
 function game() {
 
@@ -31,6 +29,8 @@ function game() {
 
     document.getElementById("survey-current-question").innerHTML = questionHTML;
 
+    goToNext = questionNumber++;   
+
 
 
     }
@@ -38,4 +38,50 @@ function game() {
 }
 
 
-game();
+game(); */
+
+ document.getElementById('submit').addEventListener('click', nextQuestion);
+
+ var q1 = document.querySelector('#q1');
+ var q2 = document.querySelector('#q2');
+ var q3 = document.querySelector('#q3');
+ var q4 = document.querySelector('#q4');
+
+var questionNumber = 1;
+// let questionList = [q1, q2, q3, q4];
+
+// for (let q=0; q<questionList.length; q++) {
+
+// }
+
+
+
+// let nextQuestion {questionIncrementer = 0, questionIncrementer++}; 
+
+// console.log('button pressed');
+// console.log(questionIncrementer);
+
+
+function nextQuestion () {
+    q1.style.display = "none";
+    q2.style.display = "block";
+    
+} //also nextQuestion () {
+//     q2.style.display = "none";
+//     q3.style.display = "block";
+    
+// } then  nextQuestion () {
+//     q3.style.display = "none";
+//     q4.style.display = "block";
+    
+// } 
+
+let i = q1, j = q2, k = q3, l = q4;
+
+function ensureRadioValue() {
+    var ele = document.getElementsByName('free1');
+      
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked)
+        document.getElementById("result").innerHTML
+                = "hf: "+ele[i].value;}}
