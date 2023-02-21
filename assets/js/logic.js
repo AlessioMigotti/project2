684362b6a2e07a24ddc21f792a208ecde9f10dee
AlessioMigotti <migottialessiodev@gmail.com>
2023-02-21 14:30:34 +0000

    Fix logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,87 +1,88 @@
+var questions = [
+    'Question: Why are bicycles so dangerous?',
+    'Question: What happens if you are obsessed with fire safety?',
+    'Question: What happens when I recognise that you are the prick who started this and you are are in the toilet with just me and no witnesses?'
+]
 
+var answers = [];
 
+// hook into the actual question text
+const questionText = document.getElementById('question-text');
 
-/* 
+// hook into all the radio buttons
+const radio1 = document.getElementById('choice-1');
+const radio2 = document.getElementById('choice-2');
+const radio3 = document.getElementById('choice-3');
+const radio4 = document.getElementById('choice-4');
+const radio5 = document.getElementById('choice-5');
 
-function init() {
+// counter for the current question
+var currentQuestionNumber = 0;
 
-    //setup all the button listeners
+// hook into the submit button
+const submit = document.getElementById('submit');
 
-    document.getElementById('submit').addEventListener('click' goToNext)          
+function setup() {
 
-    // setup the question array
-    var questions = ["Question1 dsflkfdskjfdskjl", "question 2 sdffdsfdsfds", "question 3 sdffdsfdsfds", "question 4 sdffdsfdsfds"];
-
-
-
+    document.getElementById('submit').addEventListener('click', getChoice);
 }
-
 
 
 function game() {
-
-    
-    let i = 0;
-
-    for (questionNumber = 0; questionNumber < questions.length; questionNumber++) {
-    
-    questionHTML = `<h1>${questions[i]}</h1>`;
-
-    document.getElementById("survey-current-question").innerHTML = questionHTML;
-
-    goToNext = questionNumber++;   
+    // just display the first question
+    questionText.innerText = questions[currentQuestionNumber];
+}
 
 
+function getChoice() {
+    if ((radio1.checked == false) &&
+        (radio2.checked == false) &&
+        (radio3.checked == false) &&
+        (radio4.checked == false) &&
+        (radio5.checked == false)
+    )
+        return;
 
+    if (radio1.checked) {
+        answers.push('1');
+    } else if (radio2.checked) {
+        answers.push('2');
+    } else if (radio3.checked) {
+        answers.push('3');
+    } else if (radio4.checked) {
+        answers.push('4');
+    } else {
+        answers.push('5');
     }
+
+
+   
+    if (currentQuestionNumber < questions.length-1) {
+        currentQuestionNumber++ }
+    else {
+        showGraph();
+    }
+
+    // update question to next one
+    questionText.innerText = questions[currentQuestionNumber];
+    // reset all radio buttons
+    radio1.checked = false;
+    radio2.checked = false;
+    radio3.checked = false;
+    radio4.checked = false;
+    radio5.checked = false;
 
 }
 
 
-game(); */
+function showGraph() {
+   
+    for (i=0; i<answers.length; i++) {
+        alert(answers[i]);
+    }
+    // put your graph stuff here !!!!!!!!!!
 
- document.getElementById('submit').addEventListener('click', nextQuestion);
-
- var q1 = document.querySelector('#q1');
- var q2 = document.querySelector('#q2');
- var q3 = document.querySelector('#q3');
- var q4 = document.querySelector('#q4');
-
-var questionNumber = 1;
-// let questionList = [q1, q2, q3, q4];
-
-// for (let q=0; q<questionList.length; q++) {
-
-// }
-
-
-
-// let nextQuestion {questionIncrementer = 0, questionIncrementer++}; 
-
-// console.log('button pressed');
-// console.log(questionIncrementer);
-
-
-function nextQuestion () {
-    q1.style.display = "none";
-    q2.style.display = "block";
-    
-} //also nextQuestion () {
-//     q2.style.display = "none";
-//     q3.style.display = "block";
-    
-// } then  nextQuestion () {
-//     q3.style.display = "none";
-//     q4.style.display = "block";
-    
-// } 
-
-let i = q1, j = q2, k = q3, l = q4;
-
-function ensureRadioValue() {
-    var ele = document.getElementsByName('free1');
-      
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-        document.getElementById("result").innerHTML
-                = "hf: "+ele[i].value;}}
+}
+// The main code - i.e. the actual call to the game function
+setup();
+game();
